02c4088dded5916f4823cdf38bceff2252e09556
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-09-16 17:40:49 -0500

    Completed Dashboard testing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,8 +1,37 @@
 // Test away!
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Controls from './Controls'
 
 test("Render controls", () => {
 	render(<Controls />)
+})
+
+test("Matching snapshot created", () => {
+	expect(render(<Controls />)).toMatchSnapshot()
+})
+
+test("Check for all the controls on the page when gate is unlocked and open",  () => {
+	const state= {
+		locked: false,
+		closed: false
+	};
+
+	const { getByText } = render(
+	<Controls 
+		locked={state.locked} 
+		closed={state.closed}
+	/>
+		);
+	getByText(/close gate/i);
+	getByText(/lock gate/i);
+})
+
+test("Check for all the controls on the page when gate is closed", () => {
+	const close = jest.fn()
+	const { getByText } = render(<Controls locked={false} closed={false} toggleClosed={close}/>)
+	const closeButton = getByText(/close gate/i);
+	expect(closeButton.disabled).toBe(false);
+	fireEvent.click(closeButton);
+	expect(close).toBeCalled();
 })
