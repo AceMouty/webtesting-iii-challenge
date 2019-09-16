866292233cdc5499f42c7e1c411ff62308603d40
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-09-16 16:10:24 -0500

    Have basic test running

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,18 @@
 // Test away!
+import React from 'react'
+import { render } from '@testing-library/react';
+import Display from './Display';
+
+test("Render the Display", () => {
+	render(<Display />)
+})
+
+test("Match the current snap shot", () => {
+	expect(render(<Display />)).toMatchSnapshot();
+})
+
+test("Check the UI to make sure we are displaying open and unlocked in the dispaly", () => {
+	const { getByText } = render(<Display />);
+	getByText(/open/i);
+	getByText(/unlocked/i);
+})
