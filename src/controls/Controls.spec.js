2964452ed35b55e4568e44bfadf0a6353928de04
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-09-16 18:02:56 -0500

    Completed MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -11,27 +11,19 @@ test("Matching snapshot created", () => {
 	expect(render(<Controls />)).toMatchSnapshot()
 })
 
-test("Check for all the controls on the page when gate is unlocked and open",  () => {
-	const state= {
-		locked: false,
-		closed: false
-	};
-
-	const { getByText } = render(
-	<Controls 
-		locked={state.locked} 
-		closed={state.closed}
-	/>
-		);
-	getByText(/close gate/i);
-	getByText(/lock gate/i);
+test("Check the default state of the controls", () => {
+	const { getByText } = render(<Controls />)
+	expect(getByText(/close gate/i));
+	expect(getByText(/lock gate/i));
+	expect(getByText(/lock gate/i).disabled).toBe(true);
 })
 
-test("Check for all the controls on the page when gate is closed", () => {
-	const close = jest.fn()
-	const { getByText } = render(<Controls locked={false} closed={false} toggleClosed={close}/>)
-	const closeButton = getByText(/close gate/i);
-	expect(closeButton.disabled).toBe(false);
-	fireEvent.click(closeButton);
-	expect(close).toBeCalled();
+test("Check for open gate control on the page when gate is locked", () => {
+	const { getByText } = render(<Controls locked={true} closed={true} />)
+	expect(getByText(/open gate/i).disabled).toBe(true);
+})
+
+test("Check for lock gate controls on the page when gate is open", () => {
+	const { getByText } = render(<Controls locked={false} closed={false} />)
+	expect(getByText(/close gate/i).disabled).toBe(false);
 })
